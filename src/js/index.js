f80467c74ced85de2bae8409cef55be0952766d6
Nick Fletcher <nickeax@gmail.com>
2021-09-04 16:03:06 +1000

    Finish basic plotter.

--- src/js/index.js
+++ src/js/index.js
@@ -17,11 +17,11 @@ function XYPlotter(id) {
     this.ctx.transform(1, 0, 0, -1, this.canvas.height)
   }
 
-  this.plotPoints = function (n, xArr, yArr, color, radius = 3) {
-    for (let i = 0; i < n; i++) {
+  this.plotPoints = function (data, color, radius = 3) {
+    for (let i = 0; i < data.length; i++) {
       this.ctx.fillStyle = color
       this.ctx.beginPath()
-      this.ctx.ellipse(xArr[i], yArr[i], radius, radius, 0, 0, Math.PI * 2)
+      this.ctx.ellipse(data[i].x, data[i].y, radius, radius, 0, 0, Math.PI * 2)
       this.ctx.fill()
     }
   }
@@ -29,5 +29,23 @@ function XYPlotter(id) {
   return this
 }
 const plotter = new XYPlotter('canvas1')
+const lowerX = 0
+const upperX = 900
+const lowerY = 0
+const upperY = 900
+const dataAmount = 1000
 
-plotter.plotLine(0, 0, 50, 50, "blue")
+const dataArray = randomData(dataAmount, lowerX, upperX)
+
+plotter.plotPoints(dataArray, 'red')
+
+function randomData(n, lower, upper) {
+  let arr = []
+  for (let i = 0; i < n; i++) {
+    let dataPoint = { "x": 0, "y": 0 }
+    dataPoint.x = Math.floor(Math.random() * (upper - lower)) + lower
+    dataPoint.y = Math.floor(Math.random() * (upper - lower)) + lower
+    arr.push(dataPoint)
+  }
+  return arr
+}
