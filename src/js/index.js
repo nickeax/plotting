9d4f868539aa80403ad878ab1d06fa14a50cf4e7
Nick Fletcher <nickeax@gmail.com>
2021-09-04 16:13:12 +1000

    Simple Plotly use (y = x)

--- src/js/index.js
+++ src/js/index.js
@@ -1,51 +1,19 @@
-function XYPlotter(id) {
-  this.canvas = document.getElementById(id)
-  this.ctx = this.canvas.getContext("2d")
-  this.xMin = 0
-  this.yMin = 0
-  this.xMax = this.canvas.width
-  this.yMax = this.canvas.height
+const xValues = []
+const yValues = []
 
-  this.plotLine = function (x0, y0, x, y, color) {
-    this.ctx.moveTo(x0, y0)
-    this.ctx.lineTo(x, y)
-    this.ctx.strokeStyle = color
-    this.ctx.stroke()
-  }
-
-  this.transformXY = function () {
-    this.ctx.transform(1, 0, 0, -1, this.canvas.height)
-  }
-
-  this.plotPoints = function (data, color, radius = 3) {
-    for (let i = 0; i < data.length; i++) {
-      this.ctx.fillStyle = color
-      this.ctx.beginPath()
-      this.ctx.ellipse(data[i].x, data[i].y, radius, radius, 0, 0, Math.PI * 2)
-      this.ctx.fill()
-    }
-  }
-
-  return this
+for (let x = 0; x <= 10; x++) {
+  xValues.push(x)
+  yValues.push(x)
 }
-const plotter = new XYPlotter('canvas1')
-const lowerX = 0
-const upperX = 900
-const lowerY = 0
-const upperY = 900
-const dataAmount = 1000
 
-const dataArray = randomData(dataAmount, lowerX, upperX)
+let data = [{
+  x: xValues,
+  y: yValues,
+  mode: "lines"
+}]
 
-plotter.plotPoints(dataArray, 'red')
-
-function randomData(n, lower, upper) {
-  let arr = []
-  for (let i = 0; i < n; i++) {
-    let dataPoint = { "x": 0, "y": 0 }
-    dataPoint.x = Math.floor(Math.random() * (upper - lower)) + lower
-    dataPoint.y = Math.floor(Math.random() * (upper - lower)) + lower
-    arr.push(dataPoint)
-  }
-  return arr
+let layout = {
+  title: "y = x",
 }
+
+Plotly.newPlot("myPlot", data, layout)
